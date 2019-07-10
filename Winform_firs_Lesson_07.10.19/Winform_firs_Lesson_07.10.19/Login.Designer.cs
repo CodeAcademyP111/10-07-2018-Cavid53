d79046e48355624db3132f148d7ba0beefe80282
cavid53 <JavidJb@code.edu.az>
2019-07-10 20:56:20 +0400

    homework_login

---
+++ Winform_firs_Lesson_07.10.19/Winform_firs_Lesson_07.10.19/Login.Designer.cs
@@ -0,0 +1,149 @@
+﻿namespace Winform_firs_Lesson_07._10._19
+{
+    partial class Login
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtUSername = new System.Windows.Forms.TextBox();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.chShowPassword = new System.Windows.Forms.CheckBox();
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Label1
+            // 
+            this.Label1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.Label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Label1.ForeColor = System.Drawing.Color.Blue;
+            this.Label1.Location = new System.Drawing.Point(0, 0);
+            this.Label1.Name = "Label1";
+            this.Label1.Size = new System.Drawing.Size(617, 56);
+            this.Label1.TabIndex = 0;
+            this.Label1.Text = "Welcome";
+            this.Label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Blue;
+            this.label2.Location = new System.Drawing.Point(104, 137);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(130, 29);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Username:";
+            // 
+            // txtUSername
+            // 
+            this.txtUSername.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtUSername.Location = new System.Drawing.Point(109, 185);
+            this.txtUSername.Name = "txtUSername";
+            this.txtUSername.Size = new System.Drawing.Size(258, 38);
+            this.txtUSername.TabIndex = 2;
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPassword.Location = new System.Drawing.Point(109, 320);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(258, 38);
+            this.txtPassword.TabIndex = 4;
+            this.txtPassword.UseSystemPasswordChar = true;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.Blue;
+            this.label3.Location = new System.Drawing.Point(104, 272);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(126, 29);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Password:";
+            // 
+            // chShowPassword
+            // 
+            this.chShowPassword.AutoSize = true;
+            this.chShowPassword.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.chShowPassword.ForeColor = System.Drawing.Color.Blue;
+            this.chShowPassword.Location = new System.Drawing.Point(109, 381);
+            this.chShowPassword.Name = "chShowPassword";
+            this.chShowPassword.Size = new System.Drawing.Size(151, 24);
+            this.chShowPassword.TabIndex = 5;
+            this.chShowPassword.Text = "Show Password";
+            this.chShowPassword.UseVisualStyleBackColor = true;
+            this.chShowPassword.CheckedChanged += new System.EventHandler(this.ChShowPassword_CheckedChanged);
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.BackColor = System.Drawing.Color.RoyalBlue;
+            this.btnLogin.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLogin.ForeColor = System.Drawing.Color.Black;
+            this.btnLogin.Location = new System.Drawing.Point(109, 468);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(125, 48);
+            this.btnLogin.TabIndex = 6;
+            this.btnLogin.Text = "Login";
+            this.btnLogin.UseVisualStyleBackColor = false;
+            this.btnLogin.Click += new System.EventHandler(this.BtnLogin_Click);
+            // 
+            // Login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DeepPink;
+            this.ClientSize = new System.Drawing.Size(617, 621);
+            this.Controls.Add(this.btnLogin);
+            this.Controls.Add(this.chShowPassword);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtUSername);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.Label1);
+            this.ForeColor = System.Drawing.Color.Black;
+            this.Name = "Login";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Login";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label Label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtUSername;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.CheckBox chShowPassword;
+        private System.Windows.Forms.Button btnLogin;
+    }
+}
---
+++ Winform_firs_Lesson_07.10.19/Winform_firs_Lesson_07.10.19/Login.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Winform_firs_Lesson_07._10._19
+{
+    public partial class Login : Form
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void ChShowPassword_CheckedChanged(object sender, EventArgs e)
+        {
+            if (chShowPassword.Checked)
+            {
+                txtPassword.UseSystemPasswordChar = false;
+           
+            }
+            else
+            {
+              
+                txtPassword.UseSystemPasswordChar = true;
+            }
+        }
+
+        private void BtnLogin_Click(object sender, EventArgs e)
+        {
+            string username = txtUSername.Text;
+            string password = txtPassword.Text;
+            string enterName = "Javid";
+            string enterPassword = "cavid123";
+            
+           
+            if(username== enterName && password == enterPassword)
+            {
+               
+                MessageBox.Show("Ad ve shifre duzgundur");
+                txtUSername.Text = null;
+                txtPassword.Text = null;
+            }
+            else if(username != enterName && password == enterPassword)
+            {
+                
+                MessageBox.Show("Zehmet olmasa adinizi duzgun daxil edin");
+                txtUSername.Text = null;
+                txtPassword.Text = null;
+            }
+            else
+            {
+                
+
+                MessageBox.Show("Zehmet olmasa shifrenizi duzgun daxil edin");
+                txtUSername.Text = null;
+                txtPassword.Text = null;
+            }
+            
+
+        }
+    }
+}
